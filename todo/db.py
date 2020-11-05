e18ff8545f723e248325e45006fb36c8d8006811
Daniel CM <daniel_20589@hotmail.com>
2020-11-05 03:16:04 -0600

    primer commit

---
+++ todo/db.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+from .schema  import instructions
+
+def get_db():
+    if 'db' not in g:
+        g.db = mysql.connector.connect(
+            host= current_app.config['DATABASE_HOST'],
+            user = current_app.config['DATABASE_USER'],
+            password = current_app.config['DATABASE_PASSWORD'],
+            database = current_app.config['DATABASE']
+        )
+        g.c = g.db.cursor(dictionary=True)
+    return g.db, g.c 
+
+def close_db(e=None):
+    db =g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+def init_app(app):
+    app.teardown.appcontext(close_db)
